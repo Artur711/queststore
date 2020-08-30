64eeaa9aa63720703a7b91f32f5e6566777b1d08
Artur <arturkubiak9@gmail.com>
2020-08-30 18:08:28 +0200

    Add codecooler profile page

--- view/js/script.js
+++ view/js/script.js
@@ -1,3 +1,8 @@
+const phone = document.getElementById('phone');
+const form = document.getElementById('form');
+const password = document.getElementById('password');
+const mail = document.getElementById('mail');
+
 function openList1() {
     let list = document.getElementById("ollist");
 
@@ -6,4 +11,32 @@ function openList1() {
     }else{
         list.style.display = "none";
     }
+}
+
+form.addEventListener('input', () => {
+    if (!checkIsEmpty(phone, '0125548755')) {
+        submit_phone.removeAttribute('disabled');
+    }
+    else {
+        submit_phone.setAttribute('disabled', 'disabled');
+    }
+
+    if (!checkIsEmpty(password, 'kZj4fv8cV')) {
+        submit_password.removeAttribute('disabled');
+    }
+    else {
+        submit_password.setAttribute('disabled', 'disabled');
+    }
+
+    if (!checkIsEmpty(mail, 'domink.nowak@gamial.com')) {
+        submit_mail.removeAttribute('disabled');
+    }
+    else {
+        submit_mail.setAttribute('disabled', 'disabled');
+    }
+
+});
+
+function checkIsEmpty(name, value) {
+    return (name.value === value || name.value == null || name.value == "") ? true : false;
 }
