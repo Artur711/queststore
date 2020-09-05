09688643332b0e0b3e51beb3bf1601debe837618
Artur <arturkubiak9@gmail.com>
2020-09-05 11:14:37 +0200

    Add pages for mentor to mark items and quests

--- view/js/item.js
+++ view/js/item.js
@@ -6,3 +6,18 @@ function clickCell(index) {
     item.style.borderColor = "black";
     item.setAttribute('disabled', 'disabled');
 }
+
+function clickCellByMenthor(index) {
+    const id = "index_" + index;
+    const item = document.getElementById(id);
+    if (item.innerText === "CAN USE") {
+        item.innerText = "USED";
+        item.style.backgroundColor = "red";
+        item.style.borderColor = "black";
+    }
+    else {
+        item.innerText = "CAN USE";
+        item.style.backgroundColor = "#e2d7d7";
+        item.style.borderColor = '#73AD21';
+    }
+}
