b33e5334e698c8f087ffd5ac1689a1a6ba8a3099
Artur <arturkubiak9@gmail.com>
2020-09-18 18:45:13 +0200

    Add responsive to menu pages

--- view/js/mentor.js
+++ view/js/mentor.js
@@ -28,12 +28,6 @@ function clickCell(index) {
     }
 }
 
-function openList1() {
-    let list = document.getElementById("ollist");
-
-    if (list.style.display == "none"){
-        list.style.display = "block";
-    }else{
-        list.style.display = "none";
-    }
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
 }
--- view/js/script.js
+++ view/js/script.js
@@ -3,15 +3,8 @@ const form = document.getElementById('form');
 const password = document.getElementById('password');
 const mail = document.getElementById('mail');
 
-
-function openList1() {
-    let list = document.getElementById("ollist");
-
-    if (list.style.display == "none"){
-        list.style.display = "block";
-    }else{
-        list.style.display = "none";
-    }
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
 }
 
 form.addEventListener('input', () => {
