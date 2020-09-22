6e657c0daed61753ae12ae6b0e03f152d3a148fc
Artur <arturkubiak9@gmail.com>
2020-09-22 17:51:19 +0200

    Add responsive to add student_list

--- src/main/java/com/queststore/QuestStoreApplication.java
+++ src/main/java/com/queststore/QuestStoreApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuestStoreApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(QuestStoreApplication.class, args);
     }
-
 }
