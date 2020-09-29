4fa62976ed42a0a1caaf21a48c73065fd0d12ae0
Artur <arturkubiak9@gmail.com>
2020-09-29 13:51:57 +0200

    Add welcome page

--- src/main/java/com/queststore/controller/WelcomeController.java
+++ src/main/java/com/queststore/controller/WelcomeController.java
@@ -9,16 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
-public class MentorMenuController {
+public class WelcomeController {
 
-    @GetMapping("/mentor_menu")
-    public String getMentorMenu(Model model, @SessionAttribute("loggedUser") User loggedUser) {
-        model.addAttribute("menu", null);
-        return "mentor_menu";
+    @GetMapping("/welcome_page")
+    public String getWelcome(Model model, @SessionAttribute("loggedUser") User loggedUser) {
+        model.addAttribute("welcome", loggedUser);
+        return "welcome_page";
     }
 
     @ExceptionHandler(ServletRequestBindingException.class)
-    public String handle() {
+    public String handler() {
         return "redirect:/index";
     }
 }
