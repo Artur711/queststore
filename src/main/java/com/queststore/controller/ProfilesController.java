b47e55fa3c339d18799abd66783af78e9b484d47
Artur <arturkubiak9@gmail.com>
2020-10-02 15:18:00 +0200

    Add admin profile

--- src/main/java/com/queststore/controller/ProfilesController.java
+++ src/main/java/com/queststore/controller/ProfilesController.java
@@ -6,10 +6,7 @@ import com.queststore.service.MentorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.ServletRequestBindingException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ProfilesController {
@@ -31,8 +28,19 @@ public class ProfilesController {
     @PostMapping("/update_mentor_profile")
     public String updateMentorProfile(Model model, User user, @SessionAttribute("loggedUser") User loggedUser) {
         model.addAttribute("loggedUser", setUserUpdate(loggedUser, user));
-        mentorService.updateMentor(loggedUser);
         return "redirect:/mentor_profile";
+    }
+
+    @GetMapping("/admin_profile")
+    public String getAdminProfile(Model model, @SessionAttribute("loggedUser") User loggedUser) {
+        model.addAttribute("profile", loggedUser);
+        return "admin/admin_profile_page";
+    }
+
+    @PostMapping("/update_admin_profile")
+    public String updateAdminProfile(Model model, User user, @SessionAttribute("loggedUser") User loggedUser) {
+        model.addAttribute("loggedUser", setUserUpdate(loggedUser, user));
+        return "redirect:/admin_profile";
     }
 
     @GetMapping("/codecooler_profile")
@@ -45,7 +53,6 @@ public class ProfilesController {
     @PostMapping("/update_codecooler_profile")
     public String updateCodeCoolerProfile(Model model, User user, @SessionAttribute("loggedUser") User loggedUser) {
         model.addAttribute("loggedUser", setUserUpdate(loggedUser, user));
-        mentorService.updateMentor(loggedUser);
         return "redirect:/codecooler_profile";
     }
 
@@ -53,6 +60,7 @@ public class ProfilesController {
         loggedUser.setPhoneNumber(user.getPhoneNumber());
         loggedUser.setPassword(user.getPassword());
         loggedUser.setEmail(user.getEmail());
+        mentorService.updateMentor(loggedUser);
         return loggedUser;
     }
 
