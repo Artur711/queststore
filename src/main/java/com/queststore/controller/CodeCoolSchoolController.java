20de50d86dfdeaa2695274466c3fb974a2d3a882
Artur <arturkubiak9@gmail.com>
2020-10-29 12:45:54 +0100

    Add codecool page by admin and mentor

--- src/main/java/com/queststore/controller/CodeCoolSchoolController.java
+++ src/main/java/com/queststore/controller/CodeCoolSchoolController.java
@@ -1,5 +1,6 @@
 package com.queststore.controller;
 
+import com.queststore.service.CC_ClassService;
 import com.queststore.service.CodeCoolerService;
 import com.queststore.service.UserService;
 import org.springframework.stereotype.Controller;
@@ -9,20 +10,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/codecool")
-public class CodeCoolController {
+public class CodeCoolSchoolController {
 
     private UserService userService;
     private CodeCoolerService codeCoolerService;
+    private CC_ClassService cc_classService;
 
-    public CodeCoolController(UserService userService, CodeCoolerService codeCoolerService) {
+    public CodeCoolSchoolController(UserService userService, CodeCoolerService codeCoolerService, CC_ClassService cc_classService) {
         this.userService = userService;
         this.codeCoolerService = codeCoolerService;
+        this.cc_classService = cc_classService;
     }
 
     @GetMapping
-    public String getCodeCool(Model model) {
-        model.addAttribute("mentors", userService.getAllMentors());
-        model.addAttribute("students", codeCoolerService.getAll());
+    public String getCodeCool() {
         return "other/codecool";
     }
+
+    @GetMapping("/mentor_student")
+    public String getMentorAndStudent(Model model) {
+        model.addAttribute("mentors", userService.getAllMentors());
+        model.addAttribute("students", codeCoolerService.getAll());
+        return "other/mentors_students";
+    }
+
+    @GetMapping("/class_team")
+    public String getClassesAndTeams(Model model) {
+        model.addAttribute("classes", cc_classService.getAllClasses());
+        return "other/class_team";
+    }
 }
+
